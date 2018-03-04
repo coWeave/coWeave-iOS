40cb61e1a8aa5f706398c6b3b9df0fe0aaa7c751
Benoît Frisch <benoit@fresh.lu>
2018-03-04 21:49:16 +0100

    Add UI Tests Add Accessibility Labels in Storyboard

--- coWeaveUITests/coWeaveUITests.swift
+++ coWeaveUITests/coWeaveUITests.swift
@@ -20,28 +20,85 @@
 import XCTest
 
 class coWeaveUITests: XCTestCase {
-
+    let app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
-
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        app.terminate()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testOpenView() {
+        let open = app.buttons["Open"]
+        XCTAssertTrue(open.exists)
+        
+        open.tap()
+        XCTAssertTrue(app.navigationBars["Unassigned documents"].exists)
     }
-
+    
+    func testAddCamera() {
+        let add = app.buttons["Add"]
+        let camera = app.buttons["Camera"]
+        let noCamera = app.alerts["Error"].buttons["Close"]
+        XCTAssertTrue(add.exists)
+        
+        add.tap()
+        
+        XCTAssertTrue(camera.exists)
+        camera.tap()
+        XCTAssertTrue(noCamera.exists)
+        noCamera.tap()
+        XCTAssertTrue(camera.exists)
+    }
+    
+    func testAddGallery() {
+        let add = app.buttons["Add"]
+        let gallery = app.buttons["Gallery"]
+        let photos = app.navigationBars["Photos"]
+        let closeButton = app.navigationBars.buttons.element(boundBy: 0)
+        XCTAssertTrue(add.exists)
+        
+        add.tap()
+        
+        XCTAssertTrue(gallery.exists)
+        gallery.tap()
+        waitForElementToAppear(photos)
+        XCTAssertTrue(photos.exists)
+        XCTAssertTrue(closeButton.exists)
+        closeButton.tap()
+        XCTAssertTrue(gallery.exists)
+    }
+    
+    func testAddAudio() {
+        let add = app.buttons["Add"]
+        let micro = app.buttons["Microphone"]
+        let remove = app.buttons["Delete"]
+        let audio = app.alerts["Playing recorded audio..."].buttons["Save"]
+        
+        XCTAssertTrue(add.exists)
+        
+        add.tap()
+        
+        XCTAssertTrue(micro.exists)
+        micro.tap()
+        micro.tap()
+        XCTAssertTrue(audio.exists)
+        audio.tap()
+        micro.tap()
+        micro.tap()
+        XCTAssertFalse(audio.exists)
+        XCTAssertTrue(remove.exists)
+        remove.tap()
+    }
+    
+    func waitForElementToAppear(_ element: XCUIElement) {
+        let existsPredicate = NSPredicate(format: "exists == true")
+        expectation(for: existsPredicate, evaluatedWith: element, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+    }
 }
